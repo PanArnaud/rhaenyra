ef00c6be28efa39bd21509ac2ab95cd6ae28e780
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2022-08-25 21:23:14 +0400

    add showcase and purgecss

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
   return src('rhaenyra/**/*.scss')
     .pipe(sass())
+    .pipe(purgecss({
+      content: ['*.html', 'showcase/*.html']
+    }))
     .pipe(dest('css'))
 }
 
